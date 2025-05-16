cf14d6cebba22701558e25c893fa658d75bb3549
accawsdevops <acc.aws.devops@gmail.com>
2025-05-16 07:12:58 +0530

    test

--- script/install.sh
+++ script/install.sh
@@ -5,7 +5,7 @@ sudo rm -rf /usr/share/nginx/html/*
 
 echo "Copying new build files to Nginx directory..."
 sudo cp -r /home/ec2-user/nodejsbuild/build/* /usr/share/nginx/html/
-
+#.
 echo "Restarting Nginx..."
 sudo systemctl start nginx
 sudo systemctl enable nginx
